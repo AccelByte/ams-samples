f7a51223ce81a5d110938f81301f9c73c69ef360
Bjorn Hansen <bjorn.hansen@accelbyte.net>
2025-01-07 14:53:10 -0800

    enable "local server" mode where the matchmaker returns a static ip and port.  Good for testing client interaction with the matchmaking server without have to set up a fleet.

--- matchmaking-server/server.py
+++ matchmaking-server/server.py
@@ -19,8 +19,10 @@ import http
 # these environment variables are optional:
 # CLAIM_KEYS (default: "default")
 # REGIONS (default: "us-west-2, us-east-1")
+# LOCAL_SERVER (set this to the IP:PORT of a local server to always return that server's IP:PORT to clients instead of claiming a server from AMS)
 default_claim_keys = os.environ.get("CLAIM_KEYS", "default").split(",")
 default_regions = os.environ.get("REGIONS", "us-west-2, us-east-1").split(",")
+localServer = os.environ.get("LOCAL_SERVER")
 
 CONNECTIONS = set()
 stop = False
@@ -35,6 +37,7 @@ async def register(websocket):
     print("New connection")
     CONNECTIONS.add(websocket)
     try:
+        await websocket.send("Waiting for match...")
         await websocket.wait_closed()
     finally:
         CONNECTIONS.remove(websocket)
@@ -42,28 +45,32 @@ async def register(websocket):
 
 async def matchmaker():
     while not stop:
-        while len(CONNECTIONS) >= 2:
+        match_size = 2
+        while len(CONNECTIONS) >= match_size:
             print("Match found! Requesting server...")
-            next_two = list(CONNECTIONS)[:2]
-            broadcast(next_two, "Match found! Requesting server...")
+            matched_clients = list(CONNECTIONS)[:match_size]
+            broadcast(matched_clients, "Match found! Requesting server...")
             # normally, the ordered list of regions to try to get a server from would come from the game client's matchmaking request
             # and is based on client ping times to each region.  
             # For this example, we keep the matchmaking logic super simple and just use a default list of regions
             host_port = claim(default_claim_keys, default_regions)
             if not host_port:
                 print("No server available. Waiting...")
-                broadcast(next_two, "No server available. Waiting...")
+                broadcast(matched_clients, "No server available. Waiting...")
                 break
             print("Server found! Connecting players...")
-            broadcast(next_two, host_port)
+            broadcast(matched_clients, host_port)
             await asyncio.sleep(0.1)  # so the message gets sent before closing the connection
-            for ws in next_two:
+            for ws in matched_clients:
                 await ws.close()
 
         await asyncio.sleep(2)
 
 
 def claim(claim_keys, regions, session_id="none"):
+    if localServer:
+        return localServer
+    
     body = ApiFleetClaimByKeysReq().with_claim_keys(claim_keys).with_regions(regions).with_session_id(session_id)
     result, err = fleet_claim_by_keys(body=body)
     print(result, err)
@@ -79,10 +86,11 @@ async def main():
     signal.signal(signal.SIGTERM, sigterm_handler)
     accelbyte_py_sdk.initialize()
 
-    _, error = login_client()
-    if error:
-        print(error)
-        exit(1)
+    if not localServer:
+        _, error = login_client()
+        if error:
+            print(error)
+            exit(1)
 
     port = os.environ.get("PORT", 8080)
 
